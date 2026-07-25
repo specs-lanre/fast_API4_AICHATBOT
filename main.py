d155fa6e3c9e22a1ccbbf1df26c4151a253b2732
specs-lanre <secure.usertech@protonmail.com>
2026-07-25 13:14:29 +0100

    Corrected error in json format in main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello World this is and upgrade version 002"
+    return {"message": "Hello World this is and upgrade version 002",
 		     "status" : "Active"}
 
 @app.get("/search")
